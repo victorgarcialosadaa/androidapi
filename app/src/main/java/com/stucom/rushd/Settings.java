f700b707f1ebefdb70a28c6951fb87ddddc50a6f
Victor Garcia <vgarcialosada@gmail.com>
2019-01-16 16:00:23 +0100

    16/01/19

--- app/src/main/java/com/stucom/rushd/Settings.java
+++ app/src/main/java/com/stucom/rushd/Settings.java
@@ -2,23 +2,41 @@ package com.stucom.rushd;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.stucom.rushd.api.APIResponse;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Settings extends Activity {
 
@@ -28,7 +46,7 @@ public class Settings extends Activity {
 
     EditText nomText;
 
-
+    SharedPreferences prefs;
     ImageView fotoUser;
 
     String mCurrentPhotoPath;
@@ -36,11 +54,6 @@ public class Settings extends Activity {
     Uri imageUri;
 
 
-    /**
-     * Busca tots els elements per id i el guarda a les variables, també crea un picker perque al clicar a la fotografia apareixi un desplegable
-     * que permeti a l'usuari escollir la fotografia de la galeria o directament prendre la fotografia
-     * @param savedInstanceState
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,10 +64,6 @@ public class Settings extends Activity {
         fotoUser = findViewById(R.id.fotoUser);
 
 
-
-
-
-
         fotoUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,7 +72,7 @@ public class Settings extends Activity {
                 builder.setItems(R.array.options_image, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        switch(which){
+                        switch (which) {
                             case 0:
                                 Intent gallery = new Intent(Intent.ACTION_OPEN_DOCUMENT, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
                                 startActivityForResult(gallery, PICK_IMAGE);
@@ -94,49 +103,177 @@ public class Settings extends Activity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
             imageUri = data.getData();
         }
-        if(resultCode == RESULT_OK){
+        if (resultCode == RESULT_OK) {
             SharedPreferences preferences = getPreferences(MODE_PRIVATE);
 
             SharedPreferences.Editor ed = preferences.edit();
 
-            ed.putString("UserFoto",imageUri.toString());
+            ed.putString("UserFoto", imageUri.toString());
 
             ed.apply();
         }
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         SharedPreferences preferences = getPreferences(MODE_PRIVATE);
 
-        nomText.setText(preferences.getString("nom",""));
+        nomText.setText(preferences.getString("nom", ""));
         fotoUser.setImageURI(Uri.parse(preferences.getString("UserFoto", "")));
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
 
         SharedPreferences preferences = getPreferences(MODE_PRIVATE);
 
         SharedPreferences.Editor ed = preferences.edit();
 
-        ed.putString("nom",nomText.getText().toString());
+        ed.putString("nom", nomText.getText().toString());
         updateApiValues(nomText.getText().toString(),
                 (preferences.getString("UserFoto", "")));
 
         ed.apply();
     }
 
-protected void updateApiValues(String nombre,String image) {
+    protected void updateApiValues(String nombre, String image) {
+        String URL = "https://api.flx.cat/dam2game/user";
+        StringRequest request = new StringRequest
+                (Request.Method.PUT, URL,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                String json = response;
+                                Gson gson = new Gson();
+                                Type typeToken = new TypeToken<APIResponse<String>>() {
+                                }.getType();
+                                APIResponse<String> apiResponse = gson.fromJson(json, typeToken);
+                                String userToken = apiResponse.getData();
+                                //Guardar el token el el SharedPreferences
+                                prefs = PreferenceManager.getDefaultSharedPreferences(Settings.this);
+
+                                Context context = getApplicationContext();
+                                CharSequence text = "Datos Cambiados";
+                                int duration = Toast.LENGTH_SHORT;
+
+                                Toast toast = Toast.makeText(context, text, duration);
+                                toast.show();
+
+                                //Si el token no esta vacio nos va a llevar al menu principal
+                                //Sino se va a quedar en la activity actual para pedir el registro
+                                if (!userToken.isEmpty()) {
+                                    Intent intent = new Intent(Settings.this, FirstView.class);
+                                    //startActivity(intent);
+                                    Toast.makeText(Settings.this, userToken, Toast.LENGTH_LONG).show();
+                                    final SharedPreferences.Editor editor = prefs.edit();
+                                    editor.putString("token", userToken);
+                                    editor.apply();
+                                } else {
+                                    Toast.makeText(Settings.this, "Token:" + userToken, Toast.LENGTH_LONG).show();
+                                }
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        String message = error.toString();
+                        NetworkResponse response = error.networkResponse;
+                        if (response != null) {
+                            Context context = getApplicationContext();
+                            CharSequence text = response.statusCode + " " + message;
+                            int duration = Toast.LENGTH_SHORT;
+
+                            Toast toast = Toast.makeText(context, text, duration);
+                            toast.show();
+                        }
+                    }
+                }) {
+
+            @Override
+            protected Map<String, String> getParams () {
+
+                Map<String, String> params = new HashMap<>();
+                params.put("token", prefs.getString("token", ""));
+                params.put("name", nomText.toString());
+                params.put("image", imageToBase64(fotoUser.toString()));
+                return params;
+            }
+        };
+        MyVolley.getInstance(Settings.this).add(request);
+    }
+
+    protected void getUser() {
+        String URL = "https://api.flx.cat/dam2game/user";
+        StringRequest request = new StringRequest
+                (Request.Method.PUT, URL,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                String json = response;
+                                Gson gson = new Gson();
+                                Type typeToken = new TypeToken<APIResponse<String>>() {
+                                }.getType();
+                                APIResponse<String> apiResponse = gson.fromJson(json, typeToken);
+                                String userToken = apiResponse.getData();
+                                //Guardar el token el el SharedPreferences
+                                prefs = PreferenceManager.getDefaultSharedPreferences(Settings.this);
 
 
-}
+                                Context context = getApplicationContext();
+                                CharSequence text = "Datos Cambiados";
+                                int duration = Toast.LENGTH_SHORT;
+
+                                Toast toast = Toast.makeText(context, text, duration);
+                                toast.show();
+
+                                //Si el token no esta vacio nos va a llevar al menu principal
+                                //Sino se va a quedar en la activity actual para pedir el registro
+                                if (!userToken.isEmpty()) {
+                                    Intent intent = new Intent(Settings.this, FirstView.class);
+                                    //startActivity(intent);
+                                    Toast.makeText(Settings.this, userToken, Toast.LENGTH_LONG).show();
+                                    final SharedPreferences.Editor editor = prefs.edit();
+                                    editor.putString("token", userToken);
+                                    editor.apply();
+                                } else {
+                                    Toast.makeText(Settings.this, "Token:" + userToken, Toast.LENGTH_LONG).show();
+                                }
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        String message = error.toString();
+                        NetworkResponse response = error.networkResponse;
+                        if (response != null) {
+                            Context context = getApplicationContext();
+                            CharSequence text = response.statusCode + " " + message;
+                            int duration = Toast.LENGTH_SHORT;
+
+                            Toast toast = Toast.makeText(context, text, duration);
+                            toast.show();
+                        }
+                    }
+                }) {
+
+            @Override
+            protected Map<String, String> getParams () {
+
+                Map<String, String> params = new HashMap<>();
+                params.put("token", prefs.getString("token", ""));
+                return params;
+            }
+        };
+        MyVolley.getInstance(Settings.this).add(request);
+    }
+
+
     private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + "_";
@@ -148,5 +285,17 @@ protected void updateApiValues(String nombre,String image) {
         );
         mCurrentPhotoPath = image.getAbsolutePath();
         return image;
+    }
+
+    private String imageToBase64(String image) {
+        byte[] data;
+        String base64 = null;
+        try {
+            data = image.getBytes("UTf-8");
+            base64 = Base64.encodeToString(data, Base64.DEFAULT);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return base64;
     }
 }
